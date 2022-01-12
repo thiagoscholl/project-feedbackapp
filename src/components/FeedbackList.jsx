874f1d31b184658ea6cee30f51e27a0e59da3fee
Thiago Scholl <thiagosc_@hotmail.com>
2022-01-11 23:18:32 -0300

    Managing Global State

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,17 @@
+import FeedbackItem from './FeedbackItem';
+
+function FeedbackList({ feedback }) {
+  if (!feedback || feedback.lenght == 0) {
+    return <p>No Feedback yet.</p>;
+  }
+
+  return (
+    <div className='feedback-list'>
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
+}
+
+export default FeedbackList;
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet saepe officia vero incidunt facere, ut facilis veniam blanditiis impedit aliquid?',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet saepe officia vero incidunt facere, ut facilis veniam blanditiis impedit aliquid?',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet saepe officia vero incidunt facere, ut facilis veniam blanditiis impedit aliquid?',
+  },
+];
+
+export default FeedbackData;
