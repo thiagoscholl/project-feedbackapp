2b90e8dff62c72a850b53b0d25777e41203ab2e8
Thiago Scholl <thiagosc_@hotmail.com>
2022-01-12 23:43:31 -0300

    Implementing Prop Drilling for delete cards and feedback stats component added.

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
-function FeedbackList({ feedback }) {
-  if (!feedback || feedback.lenght === 0) {
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
     return <p>No Feedback yet.</p>;
   }
 
   return (
     <div className='feedback-list'>
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   );
