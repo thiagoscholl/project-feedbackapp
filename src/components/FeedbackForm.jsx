863380306fd21c07e674206434a49e68badd55b8
Thiago Scholl <thiagosc_@hotmail.com>
2022-01-14 23:21:52 -0300

    Implementing real time validation for the form text and adding RatingSelect component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react';
+import RatingSelect from './RatingSelect';
 import Card from './shared/Card';
 import Button from './shared/Button';
 
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [rating, setRating] = useState('10');
+  const [btnDisabled, setBtnDisable] = useState(true);
+  const [message, setMessage] = useState('');
 
   const handeTextChange = (e) => {
+    if (text === '') {
+      setBtnDisable(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setMessage('Must be at least 10 characters');
+      setBtnDisable(true);
+    } else {
+      setMessage(null);
+      setBtnDisable(false);
+    }
+
     setText(e.target.value);
   };
 
@@ -13,7 +28,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-        {/*colocar rating select component*/}
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className='input-group'>
           <input
             onChange={handeTextChange}
@@ -21,8 +36,12 @@ function FeedbackForm() {
             placeholder='Write a review'
             value={text}
           />
-          <Button type='submit'>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {/*condicional*/}
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   );
