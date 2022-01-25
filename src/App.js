b178c97b976f981eeec3dc340d1f9c544dd4a07d
Thiago Scholl <thiagosc_@hotmail.com>
2022-01-24 23:59:06 -0300

    Creating Routes and Links with React Router 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import FeedbackData from './data/FeedbackData';
+import AboutIconLink from './components/AboutIconLink';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -21,14 +24,30 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className='container'>
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
+
+        <AboutIconLink />
       </div>
-    </>
+    </Router>
   );
 }
 
