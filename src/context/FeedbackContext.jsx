09276e9a4e69c156f0ecb8285f81a86c6d23b2ac
Thiago Scholl <thiagosc_@hotmail.com>
2022-01-26 23:29:53 -0300

    Edit Feedback event and state

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -7,17 +7,42 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'This item is from context',
+      text: 'This item is Feedback 1',
       rating: 10,
+    },
+    {
+      id: 2,
+      text: 'This item is Feedback 2',
+      rating: 7,
+    },
+    {
+      id: 3,
+      text: 'This item is Feedback 3',
+      rating: 9,
     },
   ]);
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  //  Seleciona o item para ser modificado
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
+  //  Deletar Feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
+  //  Adicionar Feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
@@ -29,6 +54,7 @@ export const FeedbackProvider = ({ children }) => {
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
       }}
     >
       {children}
