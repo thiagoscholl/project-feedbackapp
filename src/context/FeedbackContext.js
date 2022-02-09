b3ee1740966816e428946087c3c569c49fc98038
Thiago Scholl <thiagosc_@hotmail.com>
2022-02-09 17:33:20 -0300

    Setting up JSON-server backend and fetching data

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,31 @@
 import { v4 as uuidv4 } from 'uuid';
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This item is Feedback 1',
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: 'This item is Feedback 2',
-      rating: 7,
-    },
-    {
-      id: 3,
-      text: 'This item is Feedback 3',
-      rating: 9,
-    },
-  ]);
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      'http://localhost:5000/feedback?_sort=id&_order=desc'
+    );
+
+    const data = await response.json();
+
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   //  Deletar Feedback
   const deleteFeedback = (id) => {
@@ -61,6 +61,8 @@ export const FeedbackProvider = ({ children }) => {
         feedback,
         //  feedbackedit state
         feedbackEdit,
+        // isLoading state
+        isLoading,
         //  delete feedback function
         deleteFeedback,
         //  add feedback function
